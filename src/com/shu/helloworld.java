8d2d62b01829ee17fd46b4a2550e8cbf34a6ead1
Jason <luhui_1995@163.com>
2021-05-29 15:30:01 +0800

    Hello World项目测试

---
+++ src/com/shu/helloworld.java
@@ -0,0 +1,13 @@
+package com.shu;
+
+/**
+ * @Author Jason Lu
+ * @Date 2021/5/29/0029 14:44
+ * @Version 1.0
+ */
+public class helloworld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
